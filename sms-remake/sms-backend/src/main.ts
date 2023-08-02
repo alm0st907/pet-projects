7c3261b1ce125c3cb2772e8096033e85d9616d20
Garrett Rudisill <garrett.rudisill@gmail.com>
2023-08-02 15:49:40 -0700

    working on fixing cors

--- sms-remake/sms-backend/src/main.ts
+++ sms-remake/sms-backend/src/main.ts
@@ -3,10 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors( {
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    origin: '*',
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
